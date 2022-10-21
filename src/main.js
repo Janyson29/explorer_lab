d276ae459653847c0ec2842c8c0d57a02928b6ab
Janyson Marques <janyson29@hotmail.com>
2022-10-20 21:59:15 -0300

    add_cards

--- src/main.js
+++ src/main.js
@@ -10,6 +10,8 @@ function setCardType(type) {
     visa: ["#436D99", "#2D57F2"],
     mastercard: ["#DF6F29", "#C69347"],
     paysandu: ["#018890", "#24FFF2"],
+    american: ["darkgray", "darkgray"],
+    //maestro: ["cornflowerblue", "cornflowerblue"],
     default: ["black", "gray"],
   }
   ccBgColor01.setAttribute("fill", colors[type][0]);
@@ -49,23 +51,33 @@ const cardNumber = document.querySelector("#card-number");
 const cardNumberPattern = {
   mask: [
     {
-    mask: "0000 0000 0000 0000",
-    regex: /^4\d{0,15}/,
-    cardType: "visa",
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0,15}/,
+      cardType: "visa",
     },
     {
-    mask: "0000 0000 0000 0000",
-    regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
-    cardType: "mastercard",
+      mask: "0000 0000 0000 0000",
+      regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+      cardType: "mastercard",
     },
     {
-    mask: "0000 0000 0000 0000",
-    regex: /^7\d{0,15}/,
-    cardType: "paysandu"
+      mask: "0000 0000 0000 0000",
+      regex: /^7\d{0,15}/,
+      cardType: "paysandu",
     },
     {
-    mask: "0000 0000 0000 0000",
-    cardType: "default",
+      mask: "0000 000000 00000",
+      regex: /^3[47]\d{0,13}/,
+      cardType: "american",
+    },
+    /*{
+      mask: "0000 0000 0000 0000",
+      regex: /^(?:5[0678]\d{0,2}|6304|67\d{0,2})\d{0,12}/,
+      cardtype: "maestro",
+    },*/
+    {
+      mask: "0000 0000 0000 0000",
+      cardType: "default",
     },
   ],
   dispatch: function(appended, dynamicMasked) {
